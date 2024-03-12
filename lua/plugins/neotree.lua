f51c6800433e92878ef9994f1c7603f4a0f29761
Charbel El Khoury <charbel.g.khoury@hotmail.com>
2024-03-12 07:39:03 +0100

    cmp

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,9 +7,9 @@ return
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+            "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
         config = function()
-            vim.keymap.set('n', '<leader>e', ':Neotree toggle float<CR>')
+            vim.keymap.set('n', '<C-e>', ':Neotree toggle float<CR>')
         end
     }
